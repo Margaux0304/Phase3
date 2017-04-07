fff48d91a46a136f333144fb5ec240acd0d80789
Margaux0304 <margaux0304@laposte.net>
2017-04-07 10:39:27 +0200

    djfkkgfg

--- src/main/java/fr/amu_univ/etu/App.java
+++ src/main/java/fr/amu_univ/etu/App.java
@@ -1,27 +1,31 @@
+package fr.amu_univ.etu;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
     {
         try {
-            String[] motsInutiles;
-            String chaine = "";
-            BufferedReader buff = new BufferedReader(new FileReader("motsInutiles.txt"));
-            chaine = buff.readLine();
-            motsInutiles = chaine.split(" ");
-
+            List<String> motsInutiles = new ArrayList<>();
+            BufferedReader buff = new BufferedReader(new FileReader("motsinutiles.txt"));
+            motsInutiles = buff.lines().collect(Collectors.toList());
+            BufferedReader buf = new BufferedReader(new FileReader("science-en.csv"));
+            List<String> mots = new ArrayList<>();
+            String line;
+            while((line = buf.readLine()) != null){
+                for(String str : line.split(";")){
+                    str = str.replaceAll("\"", "");
+                    if(!motsInutiles.contains(str.toLowerCase()))
+                        System.out.println(str);
+                        mots.add(str);
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
